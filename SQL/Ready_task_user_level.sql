08377b8e1164ff522f1c79b0962f72aa0a069c3c
RoshchinaAnastasia <roshchina.ana@gmail.com>
2024-10-29 17:10:42 +1000

    Обновлено решение по SQL

--- SQL/Ready_task_user_level.sql
+++ SQL/Ready_task_user_level.sql
@@ -25,23 +25,23 @@ WHERE level_id = 1
 AND skill > 799000
 AND user_name LIKE '%a%' ;								
 2. Удалить всех пользователей, у которых skill меньше 100000								
-"DELETE FROM ""user""
-WHERE skill < 100000;"								
+DELETE FROM "user"
+WHERE skill < 100000;								
 3. Вывести все данные из таблицы user в порядке убывания по полю skill 								
-"SELECT * FROM ""user""
-ORDER BY (skill) DESC;"								
+SELECT * FROM "user"
+ORDER BY (skill) DESC;							
 4. Добавить в таблицу user нового пользователя по имени Oleg, с уровнем 4 и skill =10								
-"INSERT INTO ""user"" (user_name, level_id, skill)  
-VALUES ('Oleg', 4, 10)"								
+INSERT INTO "user" (user_name, level_id, skill)  
+VALUES ('Oleg', 4, 10)"							
 5. Обновить данные в таблице user -  для пользователей с level_id меньше 2 проставить skill 2000000								
-"UPDATE  ""user""
+UPDATE  "user"
 SET skill = 2000000
-WHERE level_id < 2;"								
+WHERE level_id < 2;							
 6. Выбрать user_name всех пользователей уровня admin используя подзапрос								
-"SELECT user_name
-FROM ""user""
-WHERE level_id = (SELECT id from ""level"" WHERE level_name = 'admin');"								
+SELECT user_name
+FROM "user"
+WHERE level_id = (SELECT id from ""level"" WHERE level_name = 'admin');								
 7. Выбрать user_name всех пользователей уровня admin используя join								
-"SELECT user_name
-FROM ""user"" AS i
-INNER JOIN ""level"" AS b ON i.level_id = b.id WHERE b.Level_name = 'admin';
+SELECT user_name
+FROM "user" AS i
+INNER JOIN "level" AS b ON i.level_id = b.id WHERE b.Level_name = 'admin';
